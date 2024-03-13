87951715a5eb3ff7756a5db13ce66d9b22aa5095
Vinícius Danciguer Marafiga <marafigabusines@gmail.com>
2024-03-13 17:47:31 -0300

    json and back

--- Store/back.js
+++ Store/back.js
@@ -9,3 +9,57 @@ window.onload = function () {
     document.getElementById("idPerfil").textContent = user.id;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../Dados/store.json")
+    .then((response) => response.json())
+    .then((data) => {
+        produtos = data;
+        const produtosContainer = document.getElementById("produtos-container");
+        
+        produtos.forEach((produto, index) => {
+            const card = document.createElement("div");
+            card.className = "card";
+            card.style.width = "18rem";
+            card.style.marginRight = "10px";
+
+            const imagem = document.createElement("img");
+            imagem.src = produto.imagem;
+            imagem.className = "card-img-top";
+            
+            const cardBody = this.documentElement("div");
+            cardBody.className = "card-body";
+
+            const cardTitle = document.createElement("h5");
+            cardTitle.className = "card-title";
+            cardTitle.textContent = produto.descricao;
+
+            const cardText = document.createElement("p");
+            cardText.className = "card-text";
+            cardText.textContent = "Preço: $" + produto.preco.toFixed(2);
+
+            const btnAdicionarAoCarrinho = document.createElement("a");
+            btnAdicionarAoCarrinho.href = "#";
+            btnAdicionarAoCarrinho.className = "btn btn-primary btn-adicionar-ao-carrinho";
+            btnAdicionarAoCarrinho.textContent = "Adicionar ao carrinho";
+            btnAdicionarAoCarrinho.setAttribute("data-indice", index);
+
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+            cardBody.appendChild(btnAdicionarAoCarrinho);
+
+            card.appendChild(imagem);
+            card.appendChild(cardBody);
+
+            produtosContainer.appendChild(card);
+        })
+    })
+})
+.catch((error) => console.error("Erro ao carregar o arquivo JSON", error))
+$("#produtos-container").on("click", "btn-adicioar-ao-carrinho", function(){
+    const indexDoProduto = $(this).data("indice")
+    const produtoSelecionado = produtos[indexDoProduto]
+    let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+    carrinho.push(produtoSelecionado);
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+    alert("PRODUTO ADICIONADO AO CARRINHO")
+})
